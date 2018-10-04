5aa245791b626eab332a9f2a41126e2c882f65cb
ClaudeDev <claudecomposer@protonmail.ch>
2018-10-04 20:21:11 +0200

    fix links for github page

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
-importScripts('/cache-polyfill.js');
+importScripts('./cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('restaurantReviews').then(function(cache) {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/css/styles.css',
-                '/data/restaurants.json',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/restaurant.html',
-                '/img/1.jpg',
-                '/img/2.jpg',
-                '/img/3.jpg',
-                '/img/4.jpg',
-                '/img/5.jpg',
-                '/img/6.jpg',
-                '/img/7.jpg',
-                '/img/8.jpg',
-                '/img/9.jpg',
-                '/img/10.jpg',
+                './',
+                './index.html',
+                './css/styles.css',
+                './data/restaurants.json',
+                './js/dbhelper.js',
+                './js/main.js',
+                './js/restaurant_info.js',
+                './restaurant.html',
+                './img/1.jpg',
+                './img/2.jpg',
+                './img/3.jpg',
+                './img/4.jpg',
+                './img/5.jpg',
+                './img/6.jpg',
+                './img/7.jpg',
+                './img/8.jpg',
+                './img/9.jpg',
+                './img/10.jpg',
             ]);
         })
     );
