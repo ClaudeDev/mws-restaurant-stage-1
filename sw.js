1ef53921e0af0bddcef5d335fbc0c50191108ad5
ClaudeDev <claudecomposer@protonmail.ch>
2018-10-04 18:47:07 +0200

    add service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('restaurantReviews').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/data/restaurants.json',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/restaurant.html',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
